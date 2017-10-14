e3a437582fac9fbe9c3b865fff9909f4a1f08d28
gds2 <gdswa@outlook.com>
2017-10-14 23:52:46 +0200

    tests geschreven

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 //var should = require("should");
-// request = require("request");
+ request = require("request");
 //var expect = require("chai").expect;
 //var util = require("utill");
 var express = require('express');
@@ -12,6 +12,23 @@ user = require('../api/models/users.js');
 
 /**
  * Good weather tests for api/users
+ */
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * START THESE FIRST
+ *
+ *
+ *
+ *
+ *
+ *
  */
 
 describe("Post a full body to the users", function () {
@@ -35,22 +52,59 @@ describe("Post a body without a prepostion to the users", function () {
     });
 });
 
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+
+
+
+/**
+ * Created zo all the tests use the same token
+ */
+describe("Start all the other tests", function () {
+    var token = null;
+    before(function (done) {
+        request.post(
+            'http://localhost:3000/auth',
+            { json: { "username": "testuser",
+                "password": "test22" } },
+            function (error, response, body) {
+                token = body.token;
+                done()
+            }
+        );
+    });
 
 describe("Get all of the users without sending parameters", function () {
     it("Should return a json file", function (done) {
-        server.get("api/users").expect("Content-type", /json/).expect(200, done);
+        server.get("api/users").set('Authorization',  token).expect("Content-type", /json/).expect(200,done);
     });
 });
 
 describe("Get users while giving parameters", function () {
     it("Should return a json file and a 200 code", function (done) {
-        server.get("api/users?username=testuser").expect("Content-type", /json/).expect(200, done);
+        server.get("api/users?username=testuser").set('Authorization',  token).expect("Content-type", /json/).expect(200, done);
     });
 });
 
 describe("Get users by using paging", function () {
     it("Should return a json file and a 200 code", function (done) {
-        server.get("api/users/page/1").expect("Content-type", /json/).expect(200, done);
+        server.get("api/users/page/1").set('Authorization',  token).expect("Content-type", /json/).expect(200, done);
     });
 });
 
@@ -62,7 +116,7 @@ describe("Get users by using paging", function () {
 
 describe("Post  users without an username that already exists", function () {
     it("Should return a 400 code", function (done) {
-        server.post("api/users").send({
+        server.post("api/users").set('Authorization',  token).send({
             "name": "test",
             "lastname": "tester",
             "preposition" : "de",
@@ -80,31 +134,31 @@ describe("Get all of the users without a token", function () {
 
 describe("Get users while giving parameters of a user that doesnt exist", function () {
     it("Should return a json file and a 200 code", function (done) {
-        server.get("api/users?username=bladhjsgdghjdfjdsbgfjdsfjdhs").expect(404, done);
+        server.get("api/users?username=bladhjsgdghjdfjdsbgfjdsfjdhs").set('Authorization',  token).expect(404, done);
     });
 });
 
 describe("Post an empty body to the users", function () {
     it("Should return a 400 code", function (done) {
-        server.post("api/users").send({}).set('Accept', /application\/json/).expect(400,done);
+        server.post("api/users").send({}).set('Accept', /application\/json/).set('Authorization',  token).expect(400,done);
     });
 });
 
 describe("Post a wrong type of body to the users", function () {
     it("Should return a 400 code", function (done) {
-        server.post("api/users").send(",,,,,,,,,,,}").expect(400,done);
+        server.post("api/users").send(",,,,,,,,,,,}").set('Authorization',  token).expect(400,done);
     });
 });
 
 describe("Get users by using a letter instead of a number by paging", function () {
     it("Should return a json file and a 200 code", function (done) {
-        server.get("api/users/page/a").expect("Content-type", /json/).expect(200, done);
+        server.get("api/users/page/a").expect("Content-type", /json/).set('Authorization',  token).expect(200, done);
     });
 });
 
 describe("Try getting users from a page that doesnt have any users", function () {
     it("Should return a 404 code", function (done) {
-        server.get("api/users/page/1000000").expect(404, done);
+        server.get("api/users/page/1000000").set('Authorization',  token).expect(404, done);
     });
 });
 
@@ -159,26 +213,26 @@ describe("Post a full body to the ratings/update rating", function () {
             "userid":"2343",
                 "rating": 5,
             "imdb" : "123"
-        }).set('Accept', /application\/json/).expect(200,done);
+        }).set('Accept', /application\/json/).set('Authorization',  token).expect(200,done);
     });
 });
 
 
 describe("get a list of average ratings", function () {
     it("Should return a 200 code", function (done) {
-        server.get("api/ratings/average").expect(expect("Content-type", /json/).expect(200,done));
+        server.get("api/ratings/average").set('Authorization',  token).expect("Content-type", /json/).expect(200,done);
     });
 });
 
 describe("get a list of average ratings by using paging", function () {
     it("Should return a 200 code", function (done) {
-        server.get("api/ratings/average/page/1").expect(expect("Content-type", /json/).expect(200,done));
+        server.get("api/ratings/average/page/1").set('Authorization',  token).expect("Content-type", /json/).expect(200,done);
     });
 });
 
 describe("Delete a rating", function () {
     it("Should return a 200 code", function (done) {
-        server.delete("api/ratings/123").expect(200,done);
+        server.delete("api/ratings/123").set('Authorization',  token).expect(200,done);
     });
 });
 
@@ -203,15 +257,15 @@ describe("Post a rating to a movie that doesnt exist", function () {
         server.post("api/ratings").send({
             "rating": 5,
             "imdb" : "12300003400430430434343"
-        }).set('Accept', /application\/json/).expect(404,done);
+        }).set('Accept', /application\/json/).set('Authorization',  token).expect(404,done);
     });
 });
 
 describe("Post a rating that doesnt have an imdb", function () {
-    it("Should return a 400 code", function (done) {
+    it("Should return a 404 code", function (done) {
         server.post("api/ratings").send({
             "rating": 5
-        }).set('Accept', /application\/json/).expect(400,done);
+        }).set('Accept', /application\/json/).set('Authorization',  token).expect(404,done);
     });
 });
 
@@ -219,19 +273,19 @@ describe("Post a rating that doesnt have a rating amount", function () {
     it("Should return a 400 code", function (done) {
         server.post("api/ratings").send({
             "imdb": "123"
-        }).set('Accept', /application\/json/).expect(400,done);
+        }).set('Accept', /application\/json/).set('Authorization',  token).expect(400,done);
     });
 });
 
 describe("Post a wrong json format to the ratings", function () {
     it("Should return a 400 code", function (done) {
-        server.post("api/ratings").send(",,,,fds,fsf,sfsjkl;:::").set('Accept', /application\/json/).expect(400,done);
+        server.post("api/ratings").set('Authorization',  token).send(",,,,fds,fsf,sfsjkl;:::").set('Accept', /application\/json/).expect(400,done);
     });
 });
 
 describe("Post an empty json  to the ratings", function () {
     it("Should return a 400 code", function (done) {
-        server.post("api/ratings").send({}).set('Accept', /application\/json/).expect(400,done);
+        server.post("api/ratings").set('Authorization',  token).send({}).set('Accept', /application\/json/).expect(400,done);
     });
 });
 
@@ -243,14 +297,14 @@ describe("get a list of average ratings without a token", function () {
 
 describe("try getting a list of average ratings by using a page that doenst exist", function () {
     it("Should return a 404 code", function (done) {
-        server.get("api/ratings/average/page/120000").expect(404,done);
+        server.get("api/ratings/average/page/120000").set('Authorization',  token).expect(404,done);
     });
 });
 
 
 describe("try getting a list of average ratings by using a letter instead of a number at paging", function () {
     it("Should return a 200 code", function (done) {
-        server.get("api/ratings/average/page/120000").expect("Content-type", /json/).expect(200,done);
+        server.get("api/ratings/average/page/sdadadada").set('Authorization',  token).expect("Content-type", /json/).expect(200,done);
     });
 });
 
@@ -262,7 +316,7 @@ describe("Delete a rating without a token", function () {
 
 describe("Delete a rating that doesnt exist", function () {
     it("Should return a 404 code", function (done) {
-        server.delete("api/ratings/123").expect(404,done);
+        server.delete("api/ratings/123").set('Authorization',  token).expect(404,done);
     });
 });
 
@@ -273,7 +327,7 @@ describe("Post a rating with the rating amount not being a number ", function ()
             "userid":"2343",
             "rating": "5",
             "imdb" : "123"
-        }).set('Accept', /application\/json/).expect(400,done);
+        }).set('Accept', /application\/json/).set('Authorization',  token).expect(400,done);
     });
 });
 
@@ -284,7 +338,7 @@ describe("Post a rating with the rating amount being lower than 0.5 ", function 
             "userid":"2343",
             "rating": 0.4,
             "imdb" : "123"
-        }).set('Accept', /application\/json/).expect(400,done);
+        }).set('Accept', /application\/json/).set('Authorization',  token).expect(400,done);
     });
 });
 
@@ -294,7 +348,7 @@ describe("Post a rating with the rating amount being higher than 5 ", function (
             "userid":"2343",
             "rating": 6,
             "imdb" : "123"
-        }).set('Accept', /application\/json/).expect(400,done);
+        }).set('Accept', /application\/json/).set('Authorization',  token).expect(400,done);
     });
 });
 
@@ -303,19 +357,19 @@ describe("Post a rating with the rating amount being higher than 5 ", function (
  */
 describe("Try getting the movies", function () {
     it("Should return a json file and a 200 code", function (done) {
-        server.get("api/movies").expect("Content-type", /json/).expect(200, done);
+        server.get("api/movies").expect("Content-type", /json/).set('Authorization',  token).expect(200, done);
     });
 });
 
 describe("Try getting the movies by using paging", function () {
     it("Should return a json file and a 200 code", function (done) {
-        server.get("api/movies/page1").expect("Content-type", /json/).expect(200, done);
+        server.get("api/movies/page/1").expect("Content-type", /json/).set('Authorization',  token).expect(200, done);
     });
 });
 
 describe("Try getting the movies by using certain parameters in the url", function () {
     it("Should return a json file and a 200 code", function (done) {
-        server.get("api/movies?imdb=123").expect("Content-type", /json/).expect(200, done);
+        server.get("api/movies?imdb=123").expect("Content-type", /json/).set('Authorization',  token).expect(200, done);
     });
 });
 
@@ -332,19 +386,19 @@ describe("Try getting the movies without a token", function () {
 
 describe("Try getting the movies by using a page that doesnt have any data", function () {
     it("Should return a json file and a 404 code", function (done) {
-        server.get("api/movies/page/2323233").expect(404, done);
+        server.get("api/movies/page/2323233").set('Authorization',  token).expect(404, done);
     });
 });
 
 describe("Try getting the movies by using a letter as a  page number", function () {
     it("Should return a json file and a 200 code", function (done) {
-        server.get("api/movies/page/a").expect("Content-type", /json/).expect(200, done);
+        server.get("api/movies/page/a").set('Authorization',  token).expect("Content-type", /json/).expect(200, done);
     });
 });
 
 describe("Try getting a movie that doest have the parameters in the url", function () {
     it("Should return a json file and a 404 code", function (done) {
-        server.get("api/movies?imdb=12233343534535343").expect(404, done);
+        server.get("api/movies?imdb=12233343534535343").set('Authorization',  token).expect(404, done);
     });
 });
 
@@ -356,7 +410,7 @@ describe("Post a full body to the ratings", function () {
         server.post("api/ratings").send({
             "userid": "tester",
             "ratingAmount" : "5",
-            "imdb": "123"}).set('Accept', /application\/json/).expect(200,done);
+            "imdb": "123"}).set('Accept', /application\/json/).set('Authorization',  token).expect(200,done);
     });
 });
 
@@ -364,7 +418,7 @@ describe("Delete a rating using the userid and the imdb", function () {
     it("Should return an OK response", function (done) {
         server.delete("api/ratings").send({
             "userid": "tester",
-            "imdb": "123"}).set('Accept', /application\/json/).expect(200,done);
+            "imdb": "123"}).set('Accept', /application\/json/).set('Authorization',  token).expect(200,done);
     });
 });
 /**
@@ -372,7 +426,7 @@ describe("Delete a rating using the userid and the imdb", function () {
  */
 describe("Post an empty body to the ratings", function () {
     it("Should return an error 400 saying that not all variables are defined", function (done) {
-        server.post("api/ratings").send({}).set('Accept', /application\/json/).expect(400,done);
+        server.post("api/ratings").send({}).set('Authorization',  token)('Accept', /application\/json/).expect(400,done);
     });
 });
 
@@ -381,14 +435,14 @@ describe("Post a body to the ratings with a rating higher than 5", function () {
         server.post("api/ratings").send({
             "userid" : "tester",
             "ratingAmount" : "6",
-            "imdb" : "123"}).set('Accept', /application\/json/).expect(400,done);
+            "imdb" : "123"}).set('Accept', /application\/json/).set('Authorization',  token).expect(400,done);
     });
 });
 
 describe("Delete a rating by posting a userId but not an imdb id", function () {
     it("Should return an 404 not found error", function (done) {
         server.delete("api/ratings").send({
-            "userid": "tester"}).set('Accept', /application\/json/).expect(404,done);
+            "userid": "tester"}).set('Accept', /application\/json/).set('Authorization',  token).expect(404,done);
     });
 });
 /**
@@ -399,7 +453,7 @@ describe("Post a body with username and password to try to login",function () {
         server.post("/auth").send({
             "username" : "Username",
             "password" : "123456"
-        }).set('Accept',/application\/json/).expect(201,done);
+        }).set('Accept',/application\/json/).set('Authorization',  token).expect(201,done);
     })
 });
 
@@ -452,5 +506,6 @@ describe("Post a body with correct username but wrong password",function () {
         }).set('Accept',/application\/json/).expect(403,done);
     })
 });
+})
 
 
